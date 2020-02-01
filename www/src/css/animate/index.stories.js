d81e6c0ebc9c0db27c75098d04b7bd3a7737fc76
joel <iamjoel007@gmail.com>
2020-02-01 14:34:20 +0800

    add anim

---
+++ www/src/css/animate/index.stories.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+export default { title: 'CSS/动画' }
+
+export const Main = () => `
+<div>
+  <div class="block anim-bounce"></div>
+</div>
+`
+
+Main.story = {
+  name: '基础用法'
+}
