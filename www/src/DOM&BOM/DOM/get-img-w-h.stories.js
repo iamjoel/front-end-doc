3bd10561b9ac8631d2f6b161e0e1b19deaa2e8a2
joel <iamjoel007@gmail.com>
2020-02-01 21:46:40 +0800

    add get img width and height

---
+++ www/src/DOM&BOM/DOM/get-img-w-h.stories.js
@@ -0,0 +1,43 @@
+export default { 
+  title: 'DOM&BOM/获取图片的宽高',
+}
+
+export const Main = () => ({
+  data() {
+    return {
+      src: 'http://cn.vuejs.org/images/logo.png',
+      size: {width: null, height: null}
+    }
+  },
+  methods: {
+    getImageHAndW(url) {
+      let vm = this
+      let image = new Image()
+      image.src = url
+      if (image.complete) {
+        imageLoaded()
+      } else {
+        image.onload = imageLoaded
+      }
+      function imageLoaded() {
+        vm.size = {
+          width: image.width + 'px',
+          height: image.height + 'px'
+        }
+      }
+    }
+  },
+  mounted() {
+    this.getImageHAndW(this.src)
+  },
+  template: `
+<div>
+  下图的宽: {{size.width}}, 高: {{size.height}} <br>
+  <img :src="src">
+</div>
+`
+})
+
+Main.story = {
+  name: '基础用法'
+}
