a8091d6c27bacc99db138a2e37f056b058f3442b
joel <iamjoel007@gmail.com>
2020-02-02 21:48:32 +0800

    perf jquery doc

--- www/src/libs/jquery/index.stories.js
+++ www/src/libs/jquery/index.stories.js
@@ -1,15 +1,14 @@
 import doc from './README.md'
 export default { 
-  title: '第三方库/JQuery',
+  title: '第三方库/jQuery',
   parameters: {
     notes: doc,
     previewTabs: {
-      'storybook/docs/panel': {hidden: true}
     }
   }
 }
 
-export const Main = () => '<div></div>'
+export const Main = () => '<div>无</div>'
 
 Main.story = {
   name: '基础用法'
